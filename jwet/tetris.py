945b22c197e3d978374bbb2678e97dad728a688e
Tipan (Blackwell 76) <tipan.verella@gmail.com>
2023-01-27 23:11:38 -0500

    ready for pygame

--- jwet/tetris.py
+++ jwet/tetris.py
@@ -4,9 +4,8 @@
     https://medium.com/gitconnected/writing-tetris-in-python-2a16bddb5318
 """
 
-from enum import Enum, auto
+from enum import Enum
 from random import choice
-from abc import ABC
 
 
 class Colors(Enum):
@@ -23,32 +22,6 @@ class Colors(Enum):
     BLUE = (0, 130, 200)
     PURPLE = (145, 30, 180)
     GREY = (128, 128, 128)
-
-
-class Shapes(Enum):
-    """Standard Tetris shapes"""
-    BAR = auto()
-    BLOCK = auto()
-    LELL = auto()
-    RELL = auto()
-    TEE = auto()
-    ZED = auto()
-    ESS = auto()
-
-
-class Figure(ABC):
-    """Abstract Base Class for a Figure"""
-
-    def move_left():
-
-    def move_right():
-
-    def rotate():
-
-    def drops():
-    
-    def freezes():
-
 
 
 def random_color() -> Colors:
